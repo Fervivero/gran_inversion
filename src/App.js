8279c7b789e9523b91dfc8fc72ce447372563805
Fernanda Vives <121441645+Fervivero@users.noreply.github.com>
2023-07-11 19:39:52 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <div className="Container">
             <Person firstName='Jane' lastName='Doe' age={45} hairColor='Black' />
             <Person firstName='John' lastName='Smith' age={88} hairColor='Brown' />
-            <Person firstName='Juan' lastName='Perez' age={32} hairColor='Oscuro' />
+            <Person firstName='Millard' lastName='Fillmore' age={50} hairColor='Brown'/>
             <Person firstName='Maria' lastName='Smith' age={62} hairColor='Brown' />
       </div>
     </div>
